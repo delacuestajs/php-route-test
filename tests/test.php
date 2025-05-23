d329d4ade7e04307412eb14d1a08606a6d1400f5
Juanse Cuesta <jcuesta@codepwr.com>
2025-05-23 13:36:03 -0500

    added use case hints

--- tests/test.php
+++ tests/test.php
@@ -4,4 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Practice\Route;
 
+/*
+* Route manual entry can be added here...
+* Route::resource('patients.metrics', 'post', ['patient' => 1, 'metrics' => 2], ['name' => 'John Doe']);
+*/
+
 Route::handleRequest();
